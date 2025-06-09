95a3e88b8614e2eb191a6b9a906a92fdbab16d66
Debajyati Dey <ddebajyati@gmail.com>
2025-06-09 10:58:16 +0530

    Final component for /About page

---
+++ src/components/About.tsx
@@ -0,0 +1,32 @@
+import "../bootstrap/main.css";
+import type React from "react";
+import Background from "./arwes/Background.tsx";
+import AboutMePage from "./Terminal/AboutMePage.tsx";
+import { Text } from "@arwes/react";
+
+const About = (): React.JSX.Element => {
+  return (
+    <>
+      <Background />
+      <Text
+        style={{
+          color: "cyan",
+          top: "12rem",
+          margin: 0,
+          padding: 0,
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+        as="h1"
+        className="h2"
+      >
+        03 : About Me
+      </Text>
+
+      <AboutMePage />
+    </>
+  );
+};
+export default About;
