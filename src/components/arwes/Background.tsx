16ee332a982b4e8ad3153402027fa091a7f8202f
Debajyati <ddebajyati@gmail.com>
2025-08-16 17:06:56 +0530

    add onlymoving lines background type in <Background/> component

--- src/components/arwes/Background.tsx
+++ src/components/arwes/Background.tsx
@@ -3,7 +3,7 @@ import { Animator } from "@arwes/react-animator";
 import { GridLines, Dots, MovingLines } from "@arwes/react-bgs";
 
 type BackgroundProps = {
-  bgType?: "onlygrid" | "full";
+  bgType?: "onlygrid" | "full" | "onlyMovingLines";
 };
 
 const Background = ({
@@ -16,42 +16,71 @@ const Background = ({
     return () => clearTimeout(tid);
   }, [active]);
 
-  return bgType === "full" ? (
-    <Animator active={active} duration={{ enter: 1, interval: 10 }}>
-      <div
-        style={{
-          position: "fixed",
-          height: "auto",
-          inset: 0,
-          minHeight: "100%",
-          backgroundColor: "#000906",
-          backgroundImage:
-            "radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)",
-          background: "no-repeat center center fixed cover",
-        }}
-      >
-        <GridLines lineColor="hsla(180, 100%, 75%, 0.05)" distance={30} />
-        <Dots color="hsla(180, 100%, 75%, 0.05)" distance={30} />
-        <MovingLines
-          lineColor="hsla(180, 100%, 75%, 0.07)"
-          distance={30}
-          sets={20}
-        />
-      </div>
-    </Animator>
-  ) : (
-      <div
-        style={{
-          backgroundColor: "#000906",
-          backgroundImage:
-            "radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)",
-        }}
-      >
-        <GridLines
-          lineColor="hsla(180, 100%, 75%, 0.5)"
-          style={{ backgroundColor: "black" }}
-        />
-      </div>
+  if (bgType === "full") {
+    return (
+      <Animator active={active} duration={{ enter: 1, interval: 10 }}>
+        <div
+          style={{
+            position: "fixed",
+            height: "auto",
+            inset: 0,
+            minHeight: "100%",
+            backgroundColor: "#000906",
+            backgroundImage:
+              "radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)",
+            background: "no-repeat center center fixed cover",
+          }}
+        >
+          <GridLines lineColor="hsla(180, 100%, 75%, 0.05)" distance={30} />
+          <Dots color="hsla(180, 100%, 75%, 0.05)" distance={30} />
+          <MovingLines
+            lineColor="hsla(180, 100%, 75%, 0.07)"
+            distance={30}
+            sets={20}
+          />
+        </div>
+      </Animator>
+    );
+  }
+
+  if (bgType === "onlyMovingLines") {
+    return (
+      <Animator active={active} duration={{ enter: 1, interval: 10 }}>
+        <div
+          style={{
+            position: "fixed",
+            height: "auto",
+            inset: 0,
+            minHeight: "100%",
+            backgroundColor: "#000906",
+            backgroundImage:
+              "radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)",
+            background: "no-repeat center center fixed cover",
+          }}
+        >
+          <Dots color="hsla(180, 100%, 75%, 0.05)" distance={30} />
+          <MovingLines
+            lineColor="hsla(180, 100%, 75%, 0.07)"
+            distance={30}
+            sets={20}
+          />
+        </div>
+      </Animator>
+    );
+  }
+  return (
+    <div
+      style={{
+        backgroundColor: "#000906",
+        backgroundImage:
+          "radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)",
+      }}
+    >
+      <GridLines
+        lineColor="hsla(180, 100%, 75%, 0.5)"
+        style={{ backgroundColor: "black" }}
+      />
+    </div>
   );
 };
 
