86198317a02f67487b00abf4dd8d454dcf1766ce
Debajyati <ddebajyati@gmail.com>
2025-08-16 17:09:02 +0530

    add html minification plugin and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ViteMinifyPlugin } from "vite-plugin-minify"
 
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  esbuild: {
+    target: 'esnext',
+  },
+  plugins: [react(),ViteMinifyPlugin({})],
 })
